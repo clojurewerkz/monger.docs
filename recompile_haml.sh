f889735dc383cfee7089975899f94b713a0a855f
Michael S. Klishin <michael@novemberain.com>
2012-04-28 19:59:49 +0400

    Use --trace

--- recompile_haml.sh
+++ recompile_haml.sh
@@ -1,3 +1,3 @@
 #! /bin/sh
-find . -name "*.haml" -type f -print | sed -e "p;s/\.haml$/\.html/" | xargs -n2 haml
+find . -name "*.haml" -type f -print | sed -e "p;s/\.haml$/\.html/" | xargs -n2 haml --trace
 find . -name "styles.sass" -type f -print | sed -e "p;s/\.sass$/\.css/" | xargs -n2 sass
